dcebdf0a697af56e0b6b6a0e76c6c6cc4fecc8fc
Crowdbotics <team@crowdbotics.com>
2023-04-07 02:27:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "green-wind-39771",
+  url: "https://green-wind-39771.botics.co",
+  api: "https://green-wind-39771.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
